ca1db67d9246a4d3bbf30d45c0376e9ba04a3c3b
OhYeahDiamantes2 <karolbolivarn@gmail.com>
2026-09-21 23:31:20 -0500

    SÍGUEME Y TE SIGO MAMI PA LA RUMBA ES QUE NOS VAMOSSSS
    Ya hay interfaz Ö

---
+++ Assets/Scripts/SalirApp.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SalirApp : MonoBehaviour
+{
+    public void SalirDelJuego()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
