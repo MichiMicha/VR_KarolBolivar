8ebc7321f05d9cc7163e33f059cbb06a552f19b3
OhYeahDiamantes2 <karolbolivarn@gmail.com>
2026-09-25 20:55:54 -0500

    BRUTA CIEGA SORDOMUDA TORPE TASTE Y TESTARUDA- ES TODO LO QUE HE SIDO POR TI ME HE CONVERTIDOOO
    Arreglitos kdsvj

--- Assets/Scripts/SalirApp.cs
+++ Assets/Scripts/SalirApp.cs
@@ -1,10 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class SalirApp : MonoBehaviour
 {
+    public float segundosDeEspera = 2f;
+
+    private bool saliendo;
+
     public void SalirDelJuego()
     {
+        if (saliendo) return;
+        saliendo = true;
+        StartCoroutine(SalirTrasEspera());
+    }
+
+    private IEnumerator SalirTrasEspera()
+    {
         Debug.Log("Saliendo del juego...");
+        yield return new WaitForSeconds(segundosDeEspera);
+
         Application.Quit();
 
 #if UNITY_EDITOR
